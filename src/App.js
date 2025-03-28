3683e9f5d6a72886fdc1cf7cea55983417f906f1
Matthew <shin.joonchul@gmail.com>
2025-03-28 18:20:57 +0900

    major cleanup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ console.groupCollapsed('src/App.js'); console.groupEnd();
 const App = (props) => {
   console.group('App(', props, ') invoked.'); console.groupEnd();
   return (
-    <div className="App">App Page 입니다.</div>
+    <div className="App">
+      App Page 입니다.
+    </div>
   );
 }
 
