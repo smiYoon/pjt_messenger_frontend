159eda4316357748314caac32c63f55e98e21d17
sth119 <08aaa80@naver.com>
2025-04-06 19:35:55 +0900

    Register 기본적인 틀

--- src/Member/Register_member.js
+++ src/Member/Register_member.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react'
 import styles from './Register_member.module.css';
 
 const Register_member = () => {
+    const [inputId, setInputId] = useState('');
+    const [inputPasswd, setInputPasswd] = useState('');
+    const [inputName, setInputName] = useState('');
+    const [inputPhoneNum, setInputPhoneNum] = useState('');
+    const [inputEmail, setInputEmail] = useState('');
     const [inputValue, setInputValue] = useState('');
 
-    const handlerChange = (event) => {
-        setInputValue(event.target.value)
-    }
-
+    const handlerIdChange = (e) => setInputId(e.target.value);
+    const handlerPasswdChange = (e) => setInputPasswd(e.target.value);
+    const handlerNameChange = (e) => setInputName(e.target.value);
+    const handlerPhoneNumChange = (e) => setInputPhoneNum(e.target.value);
+    const handlerEmailChange = (e) => setInputEmail(e.target.value);
+    const handlerChange = (e) => setInputValue(e.target.value);
     // 번호 포멧팅 함수
     function formatPhoneNumber(tel) {
         const telStr = tel.toString();
@@ -21,51 +28,55 @@ const Register_member = () => {
                 <div>이미지</div>
             </div>
             <div className={styles.right_panel}>
-                <div>
-                    <input type='text' value={inputValue} onChange={handlerChange} placeholder='아이디' />
-                </div>
-                <div>
-                    <input type='text' value={inputValue} onChange={handlerChange} placeholder='비밀번호' />
-                </div>
-                <div>
-                    <input type='text' value={inputValue} onChange={handlerChange} placeholder='이름' />
-                    <input type='number' value={inputValue} onChange={handlerChange} placeholder='휴대폰 번호' />
-                </div>
-                <div>
-                    <input type='text' placeholder='주소' />
-                    <input type='number' placeholder='우편번호' />
-                </div>
-                <div><input type='text' value={inputValue} onChange={handlerChange} placeholder='이메일' /></div>
-                <div>
-                    <select
-                        name='Department'
-                        className=''
-                        onChange={handlerChange}
-                    >
-                        <option value="">부서</option>
-                        <option value="1">개발</option>
-                        <option value="2">운영</option>
-                        <option value="3">인사</option>
-                        <option value="4">회계</option>
-                        <option value="5">마케팅</option>
-                    </select>
-                    <select
-                        name='Position'
-                        className=''
-                        onChange={handlerChange}
-                    >
-                        <option value="">직급</option>
-                        <option value="1">팀원</option>
-                        <option value="2">팀장</option>
-                        <option value="3">부서장</option>
-                        <option value="4">CEO</option>
-                        <option value="5">인사담당자</option>
-                    </select>
-                </div>
-                <div>
-                    <div>등록</div>
-                    <div>취소</div>
-                </div>
+                {/* <div className={styles.input_box}> */}
+                    <div className={styles.single_input}>
+                        <input type='text' value={inputId} className={styles.input} onChange={handlerIdChange} placeholder='아이디' />
+                    </div>
+                    <div className={styles.single_input}>
+                        <input type='text' value={inputPasswd} className={styles.input} onChange={handlerPasswdChange} placeholder='비밀번호' />
+                    </div>
+                    <div className={styles.two_input}>
+                        <input type='text' value={inputName} className={styles.smallinput} onChange={handlerNameChange} placeholder='이름' />
+                        <input type='number' value={inputPhoneNum} className={styles.largeinput} onChange={handlerPhoneNumChange} placeholder='휴대폰 번호' />
+                    </div>
+                    <div className={styles.two_input}> 
+                        <input type='text' className={styles.largeinput} placeholder='주소' />
+                        <input type='number' className={styles.smallinput} placeholder='우편번호' />
+                    </div>
+                    <div className={styles.single_input}>
+                        <input type='text' value={inputEmail} className={styles.input} onChange={handlerEmailChange} placeholder='이메일' />
+                    </div>
+                    <div>
+                        <select
+                            name='Department'
+                            className={styles.Department}
+                            onChange={handlerChange}
+                        >
+                            <option value="">부서</option>
+                            <option value="1">개발</option>
+                            <option value="2">운영</option>
+                            <option value="3">인사</option>
+                            <option value="4">회계</option>
+                            <option value="5">마케팅</option>
+                        </select>
+                        <select
+                            name='Position'
+                            className={styles.Position}
+                            onChange={handlerChange}
+                        >
+                            <option value="">직급</option>
+                            <option value="1">팀원</option>
+                            <option value="2">팀장</option>
+                            <option value="3">부서장</option>
+                            <option value="4">CEO</option>
+                            <option value="5">인사담당자</option>
+                        </select>
+                    </div>
+                    <div>
+                        <div>등록</div>
+                        <div>취소</div>
+                    </div>
+                {/* </div> text_box */}
             </div> {/* 오른쪽 창 */}
         </div>
     )
