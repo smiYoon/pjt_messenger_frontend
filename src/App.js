1d9e367516fc9ff7d4615c76391a29d26bed4654
taeyoung9601 <rlaxoduddlqslek@naver.com>
2025-04-08 18:27:18 +0900

    채팅방 기능 구현중

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Chat_main} from './chatting';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
+import ChatRoom from './chatting/chattingroom/Chatroom';
 
 console.groupCollapsed('src/App.js'); console.groupEnd();
 
@@ -13,6 +14,7 @@ const App = () => {
         <Navbar/>
         <Routes>
           <Route path="/" element={<Chat_main />} />
+          <Route path="/chat" element={<ChatRoom/>} />
         </Routes>
       </div>
     </div>
