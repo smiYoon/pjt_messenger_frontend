a60efd73eba5442e3b669ce13e3b0e006f83b7c8
Alchon1 <chltjdfkr123@naver.com>
2025-04-10 19:13:27 +0900

    work_box 추가

--- src/Work/List.js
+++ src/Work/List.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import styles from "./List.module.css";
+import {WorkBox} from "./index.js";
 
 
 console.groupCollapsed("src/Work/List.js");console.groupEnd();
@@ -60,29 +61,33 @@ const List = () => {
             <div className={styles.pageBottom}>
                 <span className={styles.pageBottom_Box}>
                     <div>진행 예정 <span className={styles.bottomNumber}>2</span><hr className={styles.hr}/></div>
+
                     <div className={styles.bottom_background}>
-                        <button onClick={() => navigate('/work/detail')}>상세버튼</button>
+                        <WorkBox className={styles.WorkBox}/>
                     </div>
                 </span>
 
                 <span className={styles.pageBottom_Box}>
                     <div>진행중 <span className={styles.bottomNumber}>5</span><hr className={styles.hr}/></div>
+
                     <div className={styles.bottom_background}>
-                        <button onClick={() => navigate('/work/detail')}>상세버튼</button>
+                        <WorkBox className={styles.WorkBox}/>
                     </div>
                 </span>
                 
                 <span className={styles.pageBottom_Box}>
                     <div>완료 대기 <span className={styles.bottomNumber}>2</span><hr className={styles.hr}/></div>
+
                     <div className={styles.bottom_background}>
-                        <button onClick={() => navigate('/work/detail')}>상세버튼</button>
+                        <WorkBox className={styles.WorkBox}/>
                     </div>
                 </span>
 
                 <span className={styles.pageBottom_Box}>
                     <div>완료 <span className={styles.bottomNumber}>7</span><hr className={styles.hr}/></div>
+                    
                     <div className={styles.bottom_background}>
-                        <button onClick={() => navigate('/work/detail')}>상세버튼</button>
+                        <WorkBox className={styles.WorkBox}/>
                     </div>
                 </span>
             </div>
--- src/Work/index.js
+++ src/Work/index.js
@@ -4,3 +4,4 @@ console.groupCollapsed('src/Work/index.js');console.groupEnd();
 export { default as W_List } from './List';
 export { default as W_Create } from './Create';
 export { default as W_Detail } from './Detail';
+export { default as WorkBox } from './WorkBox';
