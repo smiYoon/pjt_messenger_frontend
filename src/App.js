a21d34bb0948dd5dc4b01cf7601b69994aa13630
Matthew <shin.joonchul@gmail.com>
2025-04-11 18:28:14 +0900

    App 업데이트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { Chat_main } from './chatting';
-import Navbar from './Navbar/Navbar';
 import ChatRoom from './chatting/chattingroom/Chatroom';
+import Navbar from './Navbar/Navbar';
 import { Member_register, Member_modify, Member_list } from './Member';
-import { Notice_list, Notice_update, Notice_create, Notice_detail, Feedback_boardList , Feedback_create , Feedback_update } from './Board';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { Chat_main } from './chatting';
 import { W_List, W_Create, W_Detail } from './Work/index.js';
+import { Notice_list, Notice_update, Notice_create, Notice_detail, Feedback_boardList , Feedback_create , Feedback_update } from './Board';
+import { P_List, P_Create, P_Modify } from './Project/';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 
 console.groupCollapsed('src/App.js'); console.groupEnd();
@@ -20,6 +21,13 @@ const App = () => {
           <Route path="/member/list" element={<Member_list />} />
           <Route path="/member/edit/" element={<Member_modify />} />
           <Route path="/member/register" element={<Member_register />} />
+
+          <Route path="/chat" element={<Chat_main />} />
+
+          <Route path="/work" element={<W_List/>}/>
+          <Route path="/work/create" element={<W_Create/>}/>
+          <Route path="/work/detail" element={<W_Detail/>}/>
+
           <Route path="/board/notice/create" element={<Notice_create />} />
           <Route path="/board/notice/detail" element={<Notice_detail />} />
           <Route path="/board/notice/list" element={<Notice_list />} />
@@ -27,11 +35,11 @@ const App = () => {
           <Route path="/board/feedback/list" element={<Feedback_boardList />} />
           <Route path="/board/feedback/create" element={<Feedback_create />} />
           <Route path="/board/feedback/update" element={<Feedback_update />} />
-          <Route path="/chat" element={<Chat_main />} />
-          <Route path="/chat/room" element={<ChatRoom/>} />
-          <Route path="/work" element={<W_List/>}/>
-          <Route path="/work/create" element={<W_Create/>}/>
-          <Route path="/work/detail" element={<W_Detail/>}/>
+          
+          {/* <Route path="/chat/room" element={<ChatRoom/>} /> */}
+          <Route path="/project/list" element={<P_List/>}/>
+          <Route path="/project/create" element={<P_Create/>}/>
+          <Route path="/project/edit" element={<P_Modify/>}/>
         </Routes>
       </div>
     </div>
