7b78163069dfcda742f3c161a2eefb4e7d6a25fe
NogadaCoder <pardonsinau@gmail.com>
2025-04-15 18:45:16 +0900

    메인챗 css끝
    말풍선 남음

--- src/chatting/chattingroom/mainchat/Chatting.js
+++ src/chatting/chattingroom/mainchat/Chatting.js
@@ -1,7 +1,7 @@
 import styles from './Chatting.module.css';
 import React, { useState, useRef } from 'react';
-import { IoSearch } from "react-icons/io5";
-import { MdOutlineAttachFile } from "react-icons/md";
+// import { IoSearch } from "react-icons/io5"; //돋보기?
+// import { MdOutlineAttachFile } from "react-icons/md"; //클립
 import { BsArrowUpCircleFill } from "react-icons/bs";
 
 
@@ -10,9 +10,9 @@ const Chatting = () => {
         
     const fileInputRef = useRef(null);
     
-    const handleFileRef = () => {
-        fileInputRef.current.click();
-    };
+    // const handleFileRef = () => {
+    //     fileInputRef.current.click();
+    // };
     
     const hadleFileChange = (e) => {
         const file = e.target.files[0];
@@ -29,14 +29,14 @@ const Chatting = () => {
             
             <div className={styles.mainchat}>
 
-                <div className={styles.SearchBar}>
+                {/* <div className={styles.SearchBar}>
                     <IoSearch className={styles.IoSearch}/>
                     <input className={styles.searchInput}
                         placeholder='검색 키워드를 입력해주세요.'
                         // value={search}
                         // onChange={e => setSearch(e.target.value)}
                     />
-                </div>
+                </div> */}
 
                 <div className={styles.MessageInputBox}>
                     <input
@@ -45,8 +45,8 @@ const Chatting = () => {
                     onChange={hadleFileChange}
                     style={{ display: 'none' }}
                     />
-                    <MdOutlineAttachFile className={styles.fileIcon}
-                                         onClick={handleFileRef}/>
+                    {/* <MdOutlineAttachFile className={styles.fileIcon}
+                                         onClick={handleFileRef}/> */}
 
                     <input className={styles.messageInput}/>
                     <BsArrowUpCircleFill className={styles.sendMessagIcon}/>
